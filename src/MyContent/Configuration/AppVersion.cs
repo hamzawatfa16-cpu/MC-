ac506a1994421b07b34c6bed9dcc131f35a9682f
Hamza Watfa <hamzawatfa16@gmail.com>
2026-09-10 11:29:16 +0100

    Add application version metadata

---
+++ src/MyContent/Configuration/AppVersion.cs
@@ -0,0 +1,8 @@
+namespace MyContent.Configuration;
+
+internal static class AppVersion
+{
+    public const string Current = "0.1.0";
+    public const string Repository = "hamzawatfa16-cpu/MC-";
+    public const string ReleaseAssetName = "MyContent-win-x64.zip";
+}
