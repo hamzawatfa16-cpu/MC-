e1c6a1859ee2f2633366afe913bdebcb01423c77
Hamza Watfa <hamzawatfa16@gmail.com>
2026-09-10 23:39:27 +0100

    Fix application API accessibility warning

--- src/MyContent/App.axaml.cs
+++ src/MyContent/App.axaml.cs
@@ -8,7 +8,7 @@ using MyContent.Views;
 
 namespace MyContent;
 
-public partial class App : Application
+internal partial class App : Application
 {
     public override void Initialize() => AvaloniaXamlLoader.Load(this);
 
