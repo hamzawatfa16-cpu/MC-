37961f5eac0b53c08ef02339cfd4fcaf8f1c4a72
Hamza Watfa <hamzawatfa16@gmail.com>
2026-09-10 11:39:23 +0100

    Use Windows installer as release update asset

--- src/MyContent/Configuration/AppVersion.cs
+++ src/MyContent/Configuration/AppVersion.cs
@@ -4,5 +4,5 @@ internal static class AppVersion
 {
     public const string Current = "0.1.0";
     public const string Repository = "hamzawatfa16-cpu/MC-";
-    public const string ReleaseAssetName = "MyContent-win-x64.zip";
+    public const string ReleaseAssetName = "MyContent-Setup.exe";
 }
