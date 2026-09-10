5241cdd6c351bdc3242dbf5b37df7bd5488f393c
Hamza Watfa <hamzawatfa16@gmail.com>
2026-09-10 23:40:02 +0100

    Fix legal window accessibility warning

--- src/MyContent/Views/LegalDocumentWindow.axaml.cs
+++ src/MyContent/Views/LegalDocumentWindow.axaml.cs
@@ -3,7 +3,7 @@ using Avalonia.Interactivity;
 
 namespace MyContent.Views;
 
-public partial class LegalDocumentWindow : Window
+internal partial class LegalDocumentWindow : Window
 {
     public LegalDocumentWindow(string title, string content)
     {
